3a545c56f430cbda6722dd5a25bee9a94f24d5eb
wuhaojie <w19961009@126.com>
2016-05-24 21:53:06 +0800

    add Retrofit. need to test.

--- app/src/main/java/top/wuhaojie/rxdemo/RetrofitActivity.java
+++ app/src/main/java/top/wuhaojie/rxdemo/RetrofitActivity.java
@@ -1,27 +1,26 @@
 package top.wuhaojie.rxdemo;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 
-public class MainActivity extends AppCompatActivity {
+public class RetrofitActivity extends AppCompatActivity {
 
+    @OnClick(R.id.btn_visit)
+    void visitInternet(View v) {
 
-    @OnClick(R.id.btn_retrofit)
-    void toRetrofit(View v) {
-        Intent intent = new Intent(this, RetrofitActivity.class);
-        startActivity(intent);
     }
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_retrofit);
+        ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
