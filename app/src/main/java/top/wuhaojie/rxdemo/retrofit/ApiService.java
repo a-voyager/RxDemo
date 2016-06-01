67a276f6d64d295e00d47789b94872dc391b772a
wuhaojie <w19961009@126.com>
2016-06-01 13:43:19 +0800

    add rx android callback.

--- app/src/main/java/top/wuhaojie/rxdemo/retrofit/ApiService.java
+++ app/src/main/java/top/wuhaojie/rxdemo/retrofit/ApiService.java
@@ -3,6 +3,7 @@ package top.wuhaojie.rxdemo.retrofit;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
+import rx.Observable;
 
 /**
  * Created by wuhaojie on 2016/5/24 22:25.
@@ -11,5 +12,8 @@ public interface ApiService {
 
     @GET("getIpInfo.php")
     Call<IpEntity> getIpInfo(@Query("ip") String ip);
+
+    @GET("getIpInfo.php")
+    Observable<IpEntity> getIpInfoUseRx(@Query("ip") String ip);
 
 }
--- app/src/main/java/top/wuhaojie/rxdemo/retrofit/RetrofitActivity.java
+++ app/src/main/java/top/wuhaojie/rxdemo/retrofit/RetrofitActivity.java
@@ -1,6 +1,7 @@
 package top.wuhaojie.rxdemo.retrofit;
 
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -13,7 +14,12 @@ import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import rx.Observable;
+import rx.Observer;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 import top.wuhaojie.rxdemo.R;
 
 public class RetrofitActivity extends AppCompatActivity {
@@ -41,6 +47,39 @@ public class RetrofitActivity extends AppCompatActivity {
     }
 
 
+    @OnClick(R.id.btn_visit_use_rx)
+    void visitInternetUseRx() {
+
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("http://ip.taobao.com/service/")
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create()).build();
+
+        ApiService apiService = retrofit.create(ApiService.class);
+
+        Observable<IpEntity> ipInfoUseRx = apiService.getIpInfoUseRx("114.215.144.204");
+
+        ipInfoUseRx.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Observer<IpEntity>() {
+                    @Override
+                    public void onCompleted() {
+                        Snackbar.make(RetrofitActivity.this.getWindow().getDecorView(), "完成数据读取", Snackbar.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Snackbar.make(RetrofitActivity.this.getWindow().getDecorView(), "访问失败", Snackbar.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onNext(IpEntity ipEntity) {
+                        mTextView.setText(ipEntity.toString());
+                    }
+                });
+
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -48,6 +87,7 @@ public class RetrofitActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
 
     }
 
