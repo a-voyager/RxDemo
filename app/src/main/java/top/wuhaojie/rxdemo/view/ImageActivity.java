4c1ce3f0c923ef6e5cef16d16123f493b79f2a44
wuhaojie <w19961009@126.com>
2016-06-04 22:56:25 +0800

    fix bug: DraweeView -> SimpleDraweeView.

--- app/src/main/java/top/wuhaojie/rxdemo/view/ImageActivity.java
+++ app/src/main/java/top/wuhaojie/rxdemo/view/ImageActivity.java
@@ -19,14 +19,15 @@ public class ImageActivity extends AppCompatActivity {
     void loadImg() {
         Uri uri = Uri.parse("http://7xqdz8.com1.z0.glb.clouddn.com/blog_logo.jpg");
         mDvImage.setImageURI(uri);
+
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Fresco.initialize(this);
-        ButterKnife.bind(this);
         setContentView(R.layout.activity_image);
+        ButterKnife.bind(this);
         mDvImage = (DraweeView) findViewById(R.id.dv_image);
     }
 }
